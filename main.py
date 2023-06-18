a599a186676d5d8e5cde9da68636d99366a4ce9e
gadax <13903709-xgadax@users.noreply.gitlab.com>
2023-06-18 17:12:27 +0200

    it works !

--- main.py
+++ main.py
@@ -1,33 +1,60 @@
 from fastapi import FastAPI
 import psycopg2
+import time
+# from contextlib import asynccontextmanager
 
-conn = psycopg2.connect(
-    host="postgres",
-    user="fastapi",
-    database="tvnews",
-    password="azerty"
-)
 
-cur = conn.cursor()
-
-sql = '''CREATE TABLE news(\
-id SERIAL,\
-`date` varchar(50),\
-title varchar(200),\
-url varchar(200),\
-media varchar(200));'''
-
-sql2 = '''COPY persons(first_name, last_name, dob, email)\
-FROM '/app/drought-tv-news.csv'\
-DELIMITER ','\
-CSV HEADER;'''
+# sql2 = '''COPY news(date, title, url, media)\
+# FROM '/app/drought-tv-news.csv'\
+# DELIMITER ','\
+# CSV HEADER;'''
   
-  
-cur.execute(sql)
-conn.close()
+# cur.execute(sql2)
+# conn.close()
 
+gen = {}
 
 app = FastAPI()
+
+
+@app.get("/init")
+async def start():
+    print('hhhh')
+    conn = psycopg2.connect(
+        host="postgres",
+        user="fastapi",
+        database="tvnews",
+        password="azerty"
+    )
+    cur = conn.cursor()
+
+    sql = 'SELECT datname FROM pg_database;'
+    cur.execute(sql)
+    print(cur.fetchall())
+    sql = "GRANT ALL ON database tvnews TO fastapi"
+    cur.execute(sql)
+    conn.commit()
+    sql = '''CREATE TABLE news(\
+    id SERIAL,\
+    date varchar(50),\
+    title varchar(250),\
+    url text,\
+    media varchar(200));'''
+    conn.commit()
+    cur.execute(sql)
+    sql2 = '''COPY news(date, title, url, media)\
+    FROM '/app/drought-tv-news.csv'\
+    DELIMITER ','\
+    CSV HEADER;'''
+      
+    cur.execute(sql2)
+    conn.commit()
+
+    sql = "SELECT * FROM news"
+    cur.execute(sql)
+    print(cur.fetchall())
+
+
 
 @app.get("/")
 async def ping():
@@ -36,5 +63,4 @@ async def ping():
 
 @app.get("/postgres")
 async def select():
-    
     return {"msg": "bonjour"}

3cbb0efa568dcebb67d714f1c903453afbabd21a
gadax <13903709-xgadax@users.noreply.gitlab.com>
2023-06-18 17:12:28 +0200

    test working

--- test_api.py
+++ test_api.py
@@ -3,9 +3,20 @@ from fastapi.testclient import TestClient
 
 from .main import app
 
+
 client = TestClient(app)
 
-def test_read_main():
-    response = client.get("/ping")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "pong"}
+def test_ping_return_status():
+	response = client.get("/ping")
+	assert response.status_code == 200
+
+def test_ping_return_body():
+	response = client.get("/ping")
+	assert response.json() == {"msg": "pong"}
+
+def test_news_one():
+	response = client.get("/news/1")
+	assert response.json() == [[1,"2023-04-30 12:26:00",
+	"VIDÉO - Sécheresse : la pluie peut-elle encore régler le problème ?",
+	"https://www.tf1info.fr/environnement-ecologie/video-secheresse-la-pluie-peut-elle-encore-regler-le-probleme-2255634.html","TF1"
+	]]
